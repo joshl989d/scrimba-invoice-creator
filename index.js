e8bf90aba2e5c9a73d8de9acb7e60e94e9645fdb
Josh <joshl989d@gmail.com>
2022-06-19 16:37:16 -0400

    most theming and layout done, but need to fix some issues and add fonts

---
+++ index.js
@@ -0,0 +1,73 @@
+const washCar = document.getElementById("wash-car")
+const mowLawn = document.getElementById("mow-lawn")
+const pullWeeds = document.getElementById("pull-weeds")
+const sendInvoice = document.getElementById("send-invoice")
+const services = document.getElementById("services")
+const totalPrice = document.getElementById("total-price")
+const rendrService = [
+    {
+        name: "Wash Car",
+        id: "wash-car",
+        price: 10,
+        requested: false
+    }, 
+    {
+        name: "Mow Lawn",
+        id: "mow-lawn",
+        price: 20,
+        requested: false
+    }, 
+    {
+        name: "Pull Weeds",
+        id: "pull-weeds",
+        price: 30,
+        requested: false
+    }]
+
+currentTotal = 0
+totalPrice.textContent = `\$${currentTotal}`
+
+washCar.addEventListener("click", function(){
+    if (rendrService[0].requested === false) {
+    services.innerHTML += `<span>${rendrService[0].name}</span> <span>$<span><span id="amount-0">${rendrService[0].price}</span>`
+    } 
+    else {
+        console.log("already pressed")
+    }
+    rendrService[0].requested = true
+    currentTotal += 10 
+    totalPrice.textContent = `\$${currentTotal}`
+})
+
+mowLawn.addEventListener("click", function(){
+    if (rendrService[1].requested === false) {
+    services.innerHTML += `<span id="wash-name">${rendrService[1].name}</span> <span id="dollar-1">$</span><span id="amount-1">${rendrService[1].price}</span>`
+    }
+    else {
+        console.log("already pressed")
+    }
+    rendrService[1].requested = true
+    currentTotal += 20 
+    totalPrice.textContent = `\$${currentTotal}`
+})
+
+pullWeeds.addEventListener("click", function(){
+    if (rendrService[2].requested === false) {
+    services.innerHTML += `<span>${rendrService[2].name}</span> <span>$</span><span id="amount-2">${rendrService[2].price}<span>`
+    }
+    else {
+        console.log("already clicked")
+    }
+    rendrService[2].requested = true
+    currentTotal += 30
+    totalPrice.textContent = `\$${currentTotal}`
+})
+
+sendInvoice.addEventListener("click", function(){
+    services.textContent = ""
+    rendrService[0].requested = false
+    rendrService[1].requested = false
+    rendrService[2].requested = false
+    currentTotal = 0
+    totalPrice.textContent = `\$${currentTotal}`
+})
